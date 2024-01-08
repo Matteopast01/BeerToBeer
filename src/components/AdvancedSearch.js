493cb3558c77ca70ee334381d7f3686267fe8710
Martak9 <marta.paciotti@student.univaq.it>
2024-01-08 14:17:26 +0100

    Alone grigio!! 🥺

--- src/components/AdvancedSearch.js
+++ src/components/AdvancedSearch.js
@@ -44,7 +44,11 @@ function AdvancedSearch( ) {
                 </Tooltip>
                 </div>
             <Slider
-                sx={{ color: 'black', '& .MuiSlider-thumb': { boxShadow: 'none' } }}
+                sx={{ color: 'black', '& .MuiSlider-thumb': {
+                        boxShadow: 'none',
+                        '&:hover, &:focus, &.Mui-focusVisible': {
+                            boxShadow: '0 0 0 8px rgba(240, 240, 240, 0.5)',
+                        },} }}
                 getAriaLabel={() => 'range'}
                 value={[Number(values[index].min), Number(values[index].max)]}
                 onChange={handleChange(index)}
