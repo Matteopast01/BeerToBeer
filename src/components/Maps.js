2c158ee324a376a9b8d0c496df807305802aa706
giacomo.alfani@student.univaq.it <giacomo.alfani@student.univaq.it>
2024-01-13 14:50:09 +0100

    some modifications to Maps (almost done)

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -1,34 +1,46 @@
 import React from "react";
 import GoogleMapReact from 'google-map-react';
+import {useSelector} from "react-redux";
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const MapsComponent = ({text}) => <div>{text}</div>;
 
-    // TODO: legge pubSelected: se selezionato centro il marker nel pub, altrimenti nessun marker o marker di "default"
     // TODO MATTEO: query che recupera un pub fornendo l'id
-function Maps(){
-const defaultProps = {
-    center: {
-      lat: 10.99835602,
-      lng: 77.01502627
-    },
-    zoom: 11
-  };
 
-  return (
-    <div style={{ height: '100vh', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
-      >
-        <AnyReactComponent
-          lat={59.955413}
-          lng={30.337844}
-          text="My Marker"
-        />
-      </GoogleMapReact>
-    </div>
-  );
+function Maps(){
+
+    const defaultProps = {
+        center: {
+          lat: 10.99835602,
+          lng: 77.01502627
+        },
+        zoom: 11
+      };
+
+    const pubSelected = useSelector(state => state.pub);
+    const pubSelectedProps = !!pubSelected ? {
+        center: {
+        lat: pubSelected.lat,
+        lng: pubSelected.lng
+        }
+    } : null
+
+    const pubProps = !!pubSelected ? pubSelectedProps : defaultProps
+
+      return (
+        <div style = {{ height: '73vh', width: '100%' }}>
+          <GoogleMapReact
+            bootstrapURLKeys = {{ key: "" }}
+            defaultCenter = {defaultProps.center}
+            defaultZoom = {defaultProps.zoom}
+          >
+            <MapsComponent
+              lat = {pubProps.center.lat}
+              lng = {pubProps.center.lng}
+              text = "My Marker"
+            />
+          </GoogleMapReact>
+        </div>
+      );
 }
 
 export default Maps;
