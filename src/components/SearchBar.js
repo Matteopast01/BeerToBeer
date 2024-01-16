d4c71228806984b47a08750d4e8b54a3b3454dda
Martak9 <marta.paciotti@student.univaq.it>
2024-01-16 16:28:20 +0100

    Search bar with autocomplete

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,48 +1,52 @@
 import React, { useState } from 'react';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
 
 const SearchBar = ({ onSearch }) => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [isHovered, setIsHovered] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [isHovered, setIsHovered] = useState(false);
 
-  const handleInputChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
+    const handleInputChange = (event, value) => {
+        setSearchTerm(value);
+    };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    onSearch(searchTerm);
-  };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onSearch(searchTerm);
+    };
 
-  return (
-      <form onSubmit={handleSubmit} className="search-container">
-          <input
-              type="text"
-              placeholder="Search..."
-              value={searchTerm}
-              onChange={handleInputChange}
-              className="search-input"
-              style={{
-                  width: '250px',
-                  height: '30px',
-                  backgroundColor: isHovered ? '#eaeffa' :'#fafafc',
-                  border: '1px solid',
-                  borderColor: 'lightgray',
-                  borderRadius: '5px',
-                  padding: '5px',
-                  outline: 'none',
-                  transition: 'border-color 0.3s ease-in-out',
-                  fontSize: '14px',
-                  color: '#858484',
-                  fontStyle: 'italic',
-                  fontFamily: 'Arial, sans-serif',
-              }}
-              // mouseover event
-              onMouseOver={() => setIsHovered(true)}
-              // mouseout event
-              onMouseOut={() => setIsHovered(false)}
-          />
-      </form>
-  );
+    return (
+        <form onSubmit={handleSubmit} className="search-container">
+            <Autocomplete
+                freeSolo
+                options={[]}  // Add your autocomplete options here
+                value={searchTerm}
+                onInputChange={handleInputChange}
+                renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        label="Search..."
+                        variant="outlined"
+                        style={{
+                            width: isHovered ?'400px': '380px',
+                            backgroundColor: isHovered ? '#eaeffa' : '#fafafc',
+                            borderRadius: '5px',
+                            transition: 'background-color 0.3s ease-in-out',
+                            fontSize: '14px',
+                            color: '#858484',
+                            fontStyle: 'italic',
+                            fontFamily: 'Arial, sans-serif',
+                        }}
+                        InputProps={{
+                            ...params.InputProps,
+                            onMouseOver: () => setIsHovered(true),
+                            onMouseOut: () => setIsHovered(false),
+                        }}
+                    />
+                )}
+            />
+        </form>
+    );
 };
 
 export default SearchBar;
