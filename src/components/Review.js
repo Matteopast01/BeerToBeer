6b13fafbe538196674b9f5386d256d47b2fe8056
Roberto Di Stefano <robertodistefano24@gmail.com>
2024-01-20 20:34:43 +0100

    review page

--- src/components/Review.js
+++ src/components/Review.js
@@ -9,13 +9,35 @@ import {useState} from "react";
 
 
 
-function Review({rew, answers}){
+function Review({rew, answers=[]}){
 
     // Hook
     const [answerOpened, setAnswerOpened] = useState(false)
 
 
-    const user_obj = useAsync(async () => {
+    const rew_obj = useAsync(async () => {
+        return {
+            ...rew,
+            user: await get_user(rew)
+        }
+    })
+
+    const answers_objs = useAsync(async () => {
+        const items = []
+        for(let obj of answers){
+            items.push(
+                {
+                    ...obj,
+                    user: await get_user(obj)
+                }
+            )
+        }
+        return items
+    })
+
+    // Utility Function
+
+    const get_user = async (rew)=> {
         const user_q = await get_docs_by_attribute(rew.uid_author, "User", "uid")
         let img_q
         if( user_q[0].link_img !== ""){
@@ -29,32 +51,34 @@ function Review({rew, answers}){
             ...user_q[0],
             img: img_q
         }
-
-    })
+    }
 
     // Handle Function
 
     const clickHandler = (newAnswerOpened)=>{
         setAnswerOpened(newAnswerOpened)
     }
-    const render_rew = (answer_opened=true, reply_button=true, marginLeft=0)=>{
+
+    // Render Function
+
+    const render_rew = (rew, answer_opened=true, reply_button=true, marginLeft=0)=>{
         return (
             <ListItem alignItems="flex-start" sx={{marginLeft: marginLeft.toString()+"px"}}>
                 <ListItemAvatar>
-                    <Avatar alt="Remy Sharp" src={!!user_obj ? user_obj.img: ""} />
+                    <Avatar alt="Remy Sharp" src={rew.user.img} />
                 </ListItemAvatar>
                 <div style={{display: "flex",  flexDirection: "column"}}>
                     <div style={{  display: "flex"}}>
-                        <span style={{fontWeight: "bold", fontSize: "18px"}}> {!!user_obj ? user_obj.username: null}</span>
+                        <span style={{fontWeight: "bold", fontSize: "18px"}}> {rew.user.username}</span>
                     </div>
                     <div>
-                        <p style={{marginLeft: "5px"}}> comment </p>
+                        <p style={{marginLeft: "5px"}}> {rew.review} </p>
                     </div>
                     <div>
                         <time style={{fontWeight: "lighter", fontSize: "12px"}}>{new Date(rew.date).toLocaleDateString("it",{ month: '2-digit', day: '2-digit', year: 'numeric' })}</time>
                         {!!reply_button ? <CustomButton sx={{fontSize: "11px", color: '#333333'}} text={"reply"}/> : ""}
-                        {(!!reply_button && !!answer_opened) ? <CustomButton sx={{fontSize: "11px", color: '#333333'}} text={"hide answers"} endIcon={<KeyboardArrowDownIcon/>} handleClick={()=>{ clickHandler(false)}}/> : ""}
-                        {(!!reply_button && !answer_opened) ? <CustomButton sx={{fontSize: "11px", color: '#333333'}} text={"view answers"} endIcon={<KeyboardArrowUpIcon/>} handleClick={()=>{ clickHandler(true)}}/> : "" }
+                        {(!!reply_button && !!answer_opened && answers.length > 0) ? <CustomButton sx={{fontSize: "11px", color: '#333333'}} text={"hide answers"} endIcon={<KeyboardArrowDownIcon/>} handleClick={()=>{ clickHandler(false)}}/> : ""}
+                        {(!!reply_button && !answer_opened && answers.length > 0) ? <CustomButton sx={{fontSize: "11px", color: '#333333'}} text={"view answers"} endIcon={<KeyboardArrowUpIcon/>} handleClick={()=>{ clickHandler(true)}}/> : "" }
                     </div>
                 </div>
             </ListItem>
@@ -64,10 +88,16 @@ function Review({rew, answers}){
 
     return (
     <div>
-        {render_rew(answerOpened)}
-        {!! answerOpened ? render_rew(false, false, 50): ""}
+        {!! rew_obj ? render_rew(rew_obj, answerOpened): ""}
+        {(!!answers_objs && !!answerOpened) ? answers_objs.map((answer, i) => {
+            return (
+                <div key={i}>
+                    {render_rew(answer, false, false, 50)}
+                </div>
+            );
+        }) : ""}
     </div>
-            )
+                )
 }
 
 
