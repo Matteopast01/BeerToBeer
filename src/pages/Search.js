548812408fa66c2be5296034852a2ac31766f1fc
giacomo.alfani@student.univaq.it <giacomo.alfani@student.univaq.it>
2024-01-25 00:16:11 +0100

    changed the external <div> with <>

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -7,7 +7,7 @@ import {ResultContainer} from "../components/ResultContainer";
 const Search = function (){
 
     return (
-        <div>
+        <>
             <div>
                 <Header advancedSearch/>
                 <br/>
@@ -27,7 +27,7 @@ const Search = function (){
             <div>
                 <Footer/>
             </div>
-        </div>
+        </>
     )
 }
 
