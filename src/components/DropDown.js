f83279bfc338c0cefe21a223f82c2cfa826b5bd4
giacomo.alfani@student.univaq.it <giacomo.alfani@student.univaq.it>
2024-01-26 10:29:18 +0100

    modified shadows of the dropdown and inserted a todo to find again easily the palette modifications

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,5 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { GoChevronDown } from "react-icons/go";
+import theme from "../style/palette";
+import {Divider} from "@mui/material";
 
 function DropDown({ options, value, onChange }) {
     const [isOpen, setIsOpen] = useState(false);
@@ -35,6 +37,7 @@ function DropDown({ options, value, onChange }) {
                 style={{ cursor: "pointer" }}
             >
                 {option.label}
+                {/*// TODO: metterci stato con hover per evidenziare la scelta*/}
             </div>
         );
     });
@@ -46,13 +49,15 @@ function DropDown({ options, value, onChange }) {
                 onClick={handleClick}
                 style={{ cursor: "pointer" }}
             >
-                <div className="box border rounded p-3 shadow bg-white" style={{ width: '194px', textAlign: "center" }}>
+                <div className="box border rounded p-3 shadow bg-white" style={{ // TODO: punto modificato palette
+                    width: '194px', textAlign: "center", boxShadow: `1px 1px 3px ${theme.palette.primary.dark}`}}>
                     <span>{value?.label || "Select..."}</span>
                     <GoChevronDown className="text-lg ml-2" />
                 </div>
             </div>
             {isOpen && (
-                <div className="dropdown-menu is-boxed absolute top-full">
+                <div className="dropdown-menu is-boxed absolute top-full"
+                     style={{width: '194px', boxShadow: `0px 0px 1px ${theme.palette.primary.main}`}}>
                     <div className="dropdown-content box border rounded p-3 shadow bg-white">
                         {renderedOptions}
                     </div>
