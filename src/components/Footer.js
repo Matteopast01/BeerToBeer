a018bb3fa7e842808d33af20a96762650d1360d5
giacomo.alfani@student.univaq.it <giacomo.alfani@student.univaq.it>
2024-01-26 10:47:11 +0100

    modified margin top and bottom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,14 @@ function Footer(){
         width: '100%',
         textAlign: 'center',
         fontSize: '12px',
-        padding: '8px 0'
+        padding: '8px 0',
+        marginTop: '10px',
+        marginBottom: '5px',
     };
 
     return (
         <div style={footerStyle}>
-            <b>Powered by DAPP</b>
+            <b>Powered by DAPP © 2024</b>
         </div>
     );
 }
